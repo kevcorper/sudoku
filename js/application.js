e1534012de3ec9b63cd48e24847811126bc4eb44
kevcorper <kevcorper@gmail.com>
2016-10-04 22:19:54 -0400

    add skeleton for view, board, and tile objects

--- js/application.js
+++ js/application.js
@@ -2,9 +2,29 @@ $(document).ready(function() {
 
   var boardView = new BoardView();
   var board = new Board();
+  board.createBoard();
+  boardView.renderBoard(board.createStarter());
 
-  // board.placeNewTile();
-  // board.placeNewTile();
-  // boardView.renderBoard(board.grid);
+  // $(td).on('click', function(event) {
+  //   var $target = event.target;
+
+  //   var row  = $target.parent().attr('class')[-1];
+  //   var col  = $taget.attr('class')[-1];
+  //   var tile = board.grid[row][col];
+
+  //   $(td).on('keyup', function(event) {
+  //     var input = String.fromCharCode(event.keyCode)
+
+  //     if(tile.isCorrect(input)) {
+  //       boardView.revealNumber(input, row, col);
+  //     } else {
+  //       boardView.revealX(row, col);
+  //     }
+
+  //     if (boardView.isFull()) {
+  //       boardView.finish();
+  //     }
+  //   });
+  // });
 
 });
--- js/view.js
+++ js/view.js
@@ -1,2 +1,18 @@
 var BoardView = function() {
 };
+
+BoardView.prototype.renderBoard = function(grid) {
+	//using start grad from board object
+}
+
+BoardView.prototype.revealNumber = function(input, row, col) {
+}
+
+BoardView.prototype.revealX = function(row, col) {
+}
+
+BoardView.prototype.isFull = function() {
+}
+
+BoardView.prototype.finish = function() {
+}
