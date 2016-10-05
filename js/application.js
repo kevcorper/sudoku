18494f974b3db89367522e11fe109fc11a06750d
kevcorper <kevcorper@gmail.com>
2016-10-04 20:49:28 -0400

    add basic css to visualize board

--- js/application.js
+++ js/application.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
-  for (i=0;i<)
-
   var boardView = new BoardView();
   var board = new Board();
+
   // board.placeNewTile();
   // board.placeNewTile();
   // boardView.renderBoard(board.grid);
