d770afa318d528d28e52f2974d06faf0e6832a4e
kevcorper <kevcorper@gmail.com>
2016-10-04 20:18:33 -0400

    initialize with main visual board

---
+++ js/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  for (i=0;i<)
+
+  var boardView = new BoardView();
+  var board = new Board();
+  // board.placeNewTile();
+  // board.placeNewTile();
+  // boardView.renderBoard(board.grid);
+
+});
