1046a92f2306a24f6c20f9c034a27580f82df364
kevcorper <kevcorper@gmail.com>
2016-10-18 23:21:34 -0400

    update board object with checking functionality

--- js/board.js
+++ js/board.js
@@ -1,36 +1,54 @@
 var Board = function() {
-  this.grid = [
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [new Tile(),0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-	];
+   this.grid = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+  [4, 5, 6, 7, 8, 9, 1, 2, 3] 
+  [7, 8, 9, 1, 2, 3, 4, 5, 6] 
+  [2, 3, 4, 5, 6, 7, 8, 9, 1] 
+  [5, 6, 7, 8, 9, 1, 2, 3, 4] 
+  [8, 9, 1, 2, 3, 4, 5, 6, 7] 
+  [3, 4, 5, 6, 7, 8, 9, 1, 2] 
+  [6, 7, 8, 9, 1, 2, 3, 4, 5] 
+  [9, 1, 2, 3, 4, 5, 6, 7, 8]
+  ];
 
   this.range = [1,2,3,4,5,6,7,8,9];
-};
-
-Board.prototype.createBoard = function() {	
 }
 
-Board.prototype.isCorrectHorizontal = function() {
-  for (var i = 0; i < 9; i++) {
-    if (this.grid[i].sort() !== this.range) {
-      return false;
-    }
-  } 
-  return true; 
+Board.prototype.lineIsCorrect = function(line) {
+  return line.sort() === this.range;
 }
 
-Board.prototype.isCorrectVertical = function() {  
+Board.prototype.createColumn = function(col) {
+  var vertical = [];
   for (var i = 0; i < 9; i++) {
-
+    vertical.push(this.grid[i][col]);
   }
+  return vertical;
 }
 
-Board.prototype.isCorrectBox = function() {  
+Board.prototype.createBox = function(box) {
+  var box = [];
+  var rowStart = (box/3 * 3);
+  var colStart = (box % 3 * 3);
+
+  for (var row = rowStart; row < (rowStart+3); i++) {
+    for (var col = (colStart); i2 < (colStart+3); i++) {
+      box.push(this.grid[row][col]);
+    }
+  }
+  return box;
+}
+
+Board.prototype.isCorrect = function() {
+  for (var i = 0; i < 9; i++) {
+    if (!(this.lineIsCorrect(this.grid[i])) || 
+         this.lineIsCorrect(this.createColumn(i)) || 
+         this.lineIsCorrect(this.createBox(i))) {
+      return false
+    }
+  }
+  return true
+}
+
+Board.prototype.createBoard = function() {  
 }
