4dbb46beea0650461c7fbd11fbca5fc187ae2121
kevcorper <kevcorper@gmail.com>
2016-10-18 21:42:25 -0400

    reformat and refactor board.js

--- js/board.js
+++ js/board.js
@@ -10,8 +10,27 @@ var Board = function() {
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
 	];
+
+  this.range = [1,2,3,4,5,6,7,8,9];
 };
 
-Board.prototype.createBoard = function() {
-	//create tile objects
+Board.prototype.createBoard = function() {	
+}
+
+Board.prototype.isCorrectHorizontal = function() {
+  for (var i = 0; i < 9; i++) {
+    if (this.grid[i].sort() !== this.range) {
+      return false;
+    }
+  } 
+  return true; 
+}
+
+Board.prototype.isCorrectVertical = function() {  
+  for (var i = 0; i < 9; i++) {
+
+  }
+}
+
+Board.prototype.isCorrectBox = function() {  
 }
