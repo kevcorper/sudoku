c1cd531751e25550320762db5cadad4a3f90b219
kevcorper <kevcorper@gmail.com>
2017-02-25 16:34:54 -0500

    add renderGrid functionality

--- js/application.js
+++ js/application.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   var board = new Board();
   board.createBoard();
   boardView.renderBoard(board.grid);
-  // $('#point-counter').text(board.isCorrect())
 
   $(window).click(function(e) {
     var $target     = $(event.target);
@@ -13,12 +12,12 @@ $(document).ready(function() {
     if ($target.hasClass('selected')) {
       $target.removeClass('selected');
     } else if (isTile && tileIsEmpty) {
-      $('.selected').removeClass('selected');
-      $target.addClass('selected');
+        $('.selected').removeClass('selected');
+        $target.addClass('selected');
 
-      var row  = $target.parent().attr('class').slice(-1);
-      var col  = $target.attr('class').slice(-1);
-      var tile = board.grid[row][col];
+        var row  = $target.parent().attr('class').slice(-1);
+        var col  = $target.attr('class').slice(-1);
+        var tile = board.grid[row][col];
     } else if ($('.selected') && $target.hasClass('num')) {
         var input = $target.text();
         $('.selected').text(input);
@@ -34,7 +33,7 @@ $(document).ready(function() {
           boardView.finish();
         }      
     } else {
-      $('.selected').removeClass('selected')
+        $('.selected').removeClass('selected');
     }
   });
 });
--- js/view.js
+++ js/view.js
@@ -1,8 +1,32 @@
 var BoardView = function() {
 };
 
-BoardView.prototype.renderBoard = function(grid) {
-	//using start grad from board object
+BoardView.prototype.renderBoard = function(grid, level) {
+	var revealedTiles = 0;
+
+	if (level == 'hard') {
+		revealedTiles = 20;
+	} else if (level == 'medium') {
+		revealedTiles = 30;
+	} else {
+		revealedTiles = 40;
+	}
+
+	var num = 0;
+
+	while (num < revealedTiles) {
+		var row = Math.floor(Math.random() * 9);
+		var col = Math.floor(Math.random() * 9);
+
+		var tile = '.row-' + row + ' .col-' + col;
+
+		if ($(tile).text() === '') {
+			$(tile).text(grid[row][col]);
+			$(tile).addClass('full');
+			$(tile).addClass('given');
+			num++;
+		}
+	}
 }
 
 BoardView.prototype.revealNumber = function(input, row, col) {
